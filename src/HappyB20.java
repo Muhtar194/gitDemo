6852ba6f398464ef701e1e998ee1a0be4da64671
Muhtar194 <muhtartu194@gmail.com>
2020-08-01 11:14:07 -0500

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,6 +1,7 @@
 public class HappyB20 {
     public static void main(String[] args) {
-        System.out.println("B20 is always happy");
+
         System.out.println(" I love my batch ");
+        System.out.println("I do like asia ");
     }
 }
